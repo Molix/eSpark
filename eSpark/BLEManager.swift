06e323b97ed41774e510b79eb5e9dc4106082e6f
Ale MQ <alemq@MacBook-Pro-de-Ale.local>
2017-05-09 02:16:50 -0300

    Peripheral services and characteristics discover implemented.

--- eSpark/BLEManager.swift
+++ eSpark/BLEManager.swift
@@ -9,14 +9,23 @@
 import Foundation
 import CoreBluetooth
 
+
+// Services and Characteristics
+let BS_SCRATCH_UUID = CBUUID(string: "96A12A2F-5302-4003-82CB-985BCDCCB3ED")
+let BS_SERVICE_UUID = CBUUID(string: "4253")
+
+
+
 final class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
     
     // Shared Instance
     static let shared = BLEManager()
 
     // Class variables
-    dynamic var foundPeripherals = Array<CBPeripheral>()
     private var centralManager: CBCentralManager?
+
+    var foundPeripherals = Array<CBPeripheral>()
+    private var foundCharacteristics = Array<CBCharacteristic>()
 
     private override init() {
 
@@ -44,9 +53,51 @@ final class BLEManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
 
-        foundPeripherals.append(peripheral)
+        if !foundPeripherals.contains(peripheral) {
+            foundPeripherals.append(peripheral)
         
-        // Send Notification of new peripheral found
-        NotificationCenter.default.post(name:eSparkNotifications.peripheralFound, object: nil, userInfo:nil)
+            centralManager?.connect(peripheral, options: nil)
+
+            // Send Notification of new peripheral found
+            NotificationCenter.default.post(name:eSparkNotifications.peripheralFound, object: nil, userInfo:nil)
+        }        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+        print("Peripheral Connected: \(String(describing: peripheral.name))")
+        
+        // Become peripheral's delegate
+        peripheral.delegate = self;
+
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+
+        centralManager?.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    // CBPeripheral Methods
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        for service in peripheral.services! {
+            let thisService = service as CBService
+            
+            if service.uuid == BS_SERVICE_UUID {
+                peripheral.discoverCharacteristics(nil, for: thisService)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        foundCharacteristics = service.characteristics!
+        
+        print("Found characteristics:")
+        for characteristic in foundCharacteristics {
+            
+            print("\(characteristic)")
+        }
     }
 }
